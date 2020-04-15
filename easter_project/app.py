96d1d03fbf73ac7df6cf6f0a26efecdd550cf30d
Anusha Ramdarshan <anusha.ramdarshan@gmail.com>
2020-04-15 12:45:15 +0100

    swapping spotify player for remote control to spotify app

--- easter_project/app.py
+++ easter_project/app.py
@@ -6,10 +6,21 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import requests
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-
+scope = "user-read-playback-state,user-modify-playback-state"
+token = spotipy.util.prompt_for_user_token(
+    "eosimias",
+    scope=scope,
+    client_id=None,
+    client_secret=None,
+    redirect_uri=None,
+    cache_path=None,
+    oauth_manager=None,
+    show_dialog=False,
+)
+sp = spotipy.Spotify(auth=token)
+print("logged into spotify")
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
@@ -22,9 +33,25 @@ app.layout = html.Div(
             value="spotify:playlist:5isCW7qPn5ZYmuTAzEg6Vt",
             type="text",
         ),
+        dcc.Checklist(
+            id="checklist", options=[{"label": "Playing", "value": "playing"}]
+        ),
         html.Div(id="rendered_playlist", className="playlist_container",),
+        html.Div(id="ignored", style={"display": "none"}),
     ]
 )
+
+
+@app.callback(
+    Output(component_id="ignored", component_property="children"),
+    [Input(component_id="checklist", component_property="value")],
+)
+def play_pause(checked):
+    if checked == ["playing"]:
+        sp.start_playback()
+    else:
+        sp.pause_playback()
+    return []
 
 
 @app.callback(
@@ -35,24 +62,22 @@ def update_output_div(playlist_id):
     results = sp.playlist_tracks(playlist_id)
     songs = results["items"]
     rendered = [render_song(song) for song in songs]
+    spotipy.util.prompt_for_user_token(
+        "eosimias",
+        scope=None,
+        client_id=None,
+        client_secret=None,
+        redirect_uri=None,
+        cache_path=None,
+        oauth_manager=None,
+        show_dialog=False,
+    )
+    sp.current_playback()
     return rendered
 
 
 def render_song(song):
-    return html.Div(
-        children=[
-            html.Div(children=f"{song['track']['name']}"),
-            html.Iframe(
-                src=embed_link(song),
-                width="308",
-                height="80",
-                # TODO:
-                # frameborder="0",
-                # allowtransparency="true",
-                # allow="encrypted-media",
-            ),
-        ]
-    )
+    return html.Div(children=[html.Div(children=f"{song['track']['name']}"),])
 
 
 def embed_link(song):
